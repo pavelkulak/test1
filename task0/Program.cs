d40f439070c38e3954c864f1368dee3834ba5971
pavelkulak <kulak-pavel@yandex.ru>
2023-07-04 21:26:27 +0300

    complete task0 + add gitignore

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿// Возведение в квадрат
+Console.WriteLine("Введите число"); 
+int number = int.Parse(Console.ReadLine());// int.Parse первеодит строку в число
+int result = number * number;
+Console.WriteLine("Квадрат числа = " + result);// + - склейка сообщения и переменной
+// Metanit.com 
