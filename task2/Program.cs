78f38b21b0a04d665d6b12cefe1a1d3e0edf8590
pavelkulak <kulak-pavel@yandex.ru>
2023-07-05 16:35:46 +0300

    repair task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,27 +3,31 @@
 // 5 -> Пятница
 
 Console.WriteLine("Введите число");
-string number = Console.ReadLine();
-if (number == "1")
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
 {
     Console.WriteLine("Понедельник");
 }
-if (number == "2");
+if (number == 2)
 {
     Console.WriteLine("ВТорник");
-}if (number == "3");
+}
+if (number == 3)
 {
     Console.WriteLine("Среда");
-}if (number == "4");
+}
+if (number == 4)
 {
     Console.WriteLine("Четверг");
-}if (number == "5");
+}if (number == 5)
 {
     Console.WriteLine("Пятница");
-}if (number == "6");
+}
+if (number == 6)
 {
     Console.WriteLine("Суббота");
-}if (number == "7");
+}
+if (number == 7)
 {
     Console.WriteLine("Воскресенье");
 }
