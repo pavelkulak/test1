2e41b101e4625719a2f63b9837421896e5218f53
pavelkulak <kulak-pavel@yandex.ru>
2023-07-07 19:04:42 +0300

    add new tasks with arrays

---
+++ task5F/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 22, 43, 43, 15, 61, 72, 84, 19 };
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
+
+// int max1 = Max(
+// Max(array, b1, c1),
+// Max(a2, b2, c2),
+// Max(a3, b3, c3));
+// Console.WriteLine(max);
